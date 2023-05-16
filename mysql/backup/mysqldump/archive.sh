14f648f1e1a12d5e0434aaa13880f86edc49c9e2
不忘初心 <1624717079@qq.com>
2023-05-16 11:02:23 +0800

    Update archive.sh

--- mysql/backup/mysqldump/archive.sh
+++ mysql/backup/mysqldump/archive.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # archive and migrate
 set -e
-set -x
-#archive
+
 date=$(date +'%Y%m%d')
 date2=$(date +'%Y%m')
-db="yfbi"
+db="cattle"
 backup_dir="/data/backup/db_backup/${db}"
 archive_dir="/data/ossfs/tms/${db}/${date2}"
 
 # 进行tar归档备份文件
 function pack_to_tar() {
-    if ls "${backup_dir}"/"${db}"_"${date}"*.sql.gz 1> /dev/null 2>&1;then
+    if ls ${backup_dir}/${db}_${date}*.sql.gz 1> /dev/null 2>&1;then
         echo "文件存在，开始归档"
         cd "${backup_dir}" || exit
-        tar cf "${db}"_"${date}".tar "${db}"_"${date}"*.sql.gz
+        tar cf ${db}_${date}.tar ${db}_${date}*.sql.gz
         echo "归档完成"
     else
         echo "No such directory"
@@ -24,39 +23,30 @@ function pack_to_tar() {
 
 # 创建oss中相应文件夹
 function mkdir_to_oss() {
-    if [ ! -d "${archive_dir}" ];then
+    if [ ! -d ${archive_dir} ];then
         echo "No such directory"
-        mkdir "${archive_dir}"
+        mkdir ${archive_dir}
     else
         echo "Directory exists"
     fi
 }
 
 # 传输到oss存储
-# function trans_to_oss {
-# 	cp ${db}_${date}.tar ${archive_dir}/
-#     if [ $? -eq 0 ]; then
-#         echo "====move ok!===="
-#     else
-#         echo "====move failed!===="
-#         exit 1
-#     fi
-# }
-
 function trans_to_oss {
-    if ! cp "${db}"_"${date}".tar "${archive_dir}"/
-    then
+    cp ${db}_${date}.tar ${archive_dir}/
+    if [ $? -eq 0 ]; then
+        echo "====move ok!===="
+    else
         echo "====move failed!===="
         exit 1
-    else
-        echo "====move ok!===="
     fi
 }
 
 # 清理文件
 function delete_files() {
     echo "delete files"
-    rm -f "${db}"_"${date}"*
+    cd "${backup_dir}" || exit
+    rm -vf ${db}_${date}*
 }
 
 function main() {

3114bb5aa349026ea2be7d486b3d915d727dd91d
不忘初心 <1624717079@qq.com>
2023-12-22 09:42:47 +0800

    Update dump.sh

--- mysql/backup/mysqldump/dump.sh
+++ mysql/backup/mysqldump/dump.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-## auth dump
-set -e
-#set -x
-
-db="yfbi"
-backup_dir="/data/backup/db_backup/${db}"
+#mysqldump
 
 date=$(date +%Y%m%d%H%M)
-mysqldump ${db} | gzip > ${backup_dir}/${db}_${date}.sql.gz
+olddate=$(date -d yesterday +'%Y%m%d')
+dbuser="root"
+dbpasswd="root"
+dbname="wms"
+backdir="/data/backup"
+backfile="wms_${date}.sql.gz"
+
+function backup() {
+    echo "开始备份数据库"
+    mysqldump -u"${dbuser}" -p"${dbpasswd}" "${dbname}" | gzip > "${backdir}"/"${backfile}"
+    echo "备份完成"
+}
+
+function del_old {
+    echo "开始删除旧备份"
+    cd "${backdir}" || exit
+    rm -fv wms_"${olddate}"*.sql.gz
+    echo "删除完成"
+}
+
+function main() {
+    backup
+    # del_old
+}
+
+main

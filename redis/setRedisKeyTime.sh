602dd88a753e94e700fc8b76b960c1d2d6c55675
不忘初心 <1624717079@qq.com>
2024-03-04 16:30:59 +0800

    Update setRedisKeyTime.sh

--- redis/setRedisKeyTime.sh
+++ redis/setRedisKeyTime.sh
@@ -10,6 +10,7 @@ cursor=0
 cnt=100
 new_cursor=0
 exec_time=$(date +%Y%m%d)
+redis_cli=$(pwd)/redis-cli
 log_dir=$(pwd)/redis_modify_key
 log_file="$log_dir/modify_key_$exec_time.log"
 
@@ -29,21 +30,21 @@ cleanup() {
 # 给Redis永久Key设置过期时间
 function modifyKeyTime(){
     # 使用Redis中的scan命令，以非阻塞的方式实现key值的分页查找
-    ./redis-cli -h $db_ip -p $db_port -a $password scan $cursor count $cnt > scan_tmp_result
+    ${redis_cli} -h $db_ip -p $db_port -a $password scan $cursor count $cnt > scan_tmp_result
     # 获取第一行，scan返回游标值
     new_cursor=$(sed -n '1p' scan_tmp_result)
     # 获取第二行到最后一行，100行key数据
     sed -n '2,$p' scan_tmp_result > scan_result
     cat scan_result | while read line; do
-        ttl_result=$(./redis-cli -h $db_ip -p $db_port -a $password ttl "$line")
+        ttl_result=$(${redis_cli} -h $db_ip -p $db_port -a $password ttl "$line")
         if [[ $ttl_result == -1 ]];then
             echo 'key:'"$line" >> ./redis_modify_key/modify_key_"$exec_time".log
             # 获取5位不为0开始的随机数，避免设置同一过期时间，引起缓存雪崩
             time=$(tr -cd '1-9' </dev/urandom | head -c 5)
             # 获取key的值
-            value=$(./redis-cli -h $db_ip -p $db_port -a $password get "$line")
+            value=$(${redis_cli} -h $db_ip -p $db_port -a $password get "$line")
             # 设置过期时间并记录结果
-            result=$(./redis-cli -h "$db_ip" -p "$db_port" -a "$password" expire "$line" "$time")
+            result=$(${redis_cli} -h "$db_ip" -p "$db_port" -a "$password" expire "$line" "$time")
             # 将结果写入日志文件
             {
                 echo 'value:'"$value"
@@ -55,19 +56,20 @@ function modifyKeyTime(){
 
     # 以 0 作为游标开始一次新的迭代， 一直调用 SCAN 命令， 直到命令返回游标 0 ，遍历完毕
     while [ $cursor -ne "$new_cursor" ]; do
-        ./redis-cli -h $db_ip -p $db_port -a $password scan "$new_cursor" count $cnt > scan_tmp_result
+        ${redis_cli} -h $db_ip -p $db_port -a $password scan "$new_cursor" count $cnt > scan_tmp_result
         new_cursor=$(sed -n '1p' scan_tmp_result)
         sed -n '2,$p' scan_tmp_result > scan_result
         cat scan_result | while read line; do
-            ttl_result=$(./redis-cli -h $db_ip -p $db_port -a $password ttl "$line")
+            ttl_result=$(${redis_cli} -h $db_ip -p $db_port -a $password ttl "$line")
             if [[ $ttl_result == -1 ]];then
                 echo 'key:'"$line" >> ./redis_modify_key/modify_key_"$exec_time".log
+
                 # 获取5位不为0开始的随机数，避免设置同一过期时间，引起缓存雪崩
                 time=$(tr -cd '1-9' </dev/urandom | head -c 5)
                 # 获取key的值
-                value=$(./redis-cli -h $db_ip -p $db_port -a $password get "$line")
+                value=$(${redis_cli} -h $db_ip -p $db_port -a $password get "$line")
                 # 设置过期时间并记录结果
-                result=$(./redis-cli -h "$db_ip" -p "$db_port" -a "$password" expire "$line" "$time")
+                result=$(${redis_cli} -h "$db_ip" -p "$db_port" -a "$password" expire "$line" "$time")
                 {
                     echo 'value:'"$value"
                     echo 'expire:'"$time"

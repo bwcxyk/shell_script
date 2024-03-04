1244fffd122285a75078818fe6c8b8f18291b238
不忘初心 <1624717079@qq.com>
2024-03-04 15:18:23 +0800

    Update setRedisKeyTime.sh

--- redis/setRedisKeyTime.sh
+++ redis/setRedisKeyTime.sh
@@ -7,13 +7,19 @@ cursor=0
 cnt=100
 new_cursor=0
 exec_time=$(date +%Y%m%d)
+log_dir=$(pwd)/redis_modify_key
+log_file="$log_dir/modify_key_$exec_time.log"
 
 # 创建目录
-function mkdirs(){
-    filedir=./redis_modify_key
-    if [ ! -d "$filedir" ]; then
-        mkdir -p $filedir
+mkdirs() {
+    if [ ! -d "$log_dir" ]; then
+        mkdir -p "$log_dir"
     fi
+}
+
+cleanup() {
+    rm -rf scan_tmp_result
+    rm -rf scan_result
 }
 
 # 给Redis永久Key设置过期时间
@@ -21,12 +27,11 @@ function modifyKeyTime(){
     # 使用Redis中的scan命令，以非阻塞的方式实现key值的分页查找
     ./redis-cli -h $db_ip -p $db_port -a $password scan $cursor count $cnt > scan_tmp_result
     # 获取第一行，scan返回游标值
-    new_cursor=`sed -n '1p' scan_tmp_result`
+    new_cursor=$(sed -n '1p' scan_tmp_result)
     # 获取第二行到最后一行，100行key数据
     sed -n '2,$p' scan_tmp_result > scan_result
-    cat scan_result |while read line
-    do
-        ttl_result=`./redis-cli -h $db_ip -p $db_port -a $password ttl $line`
+    while read -r line < scan_result; do
+        ttl_result=$(./redis-cli -h $db_ip -p $db_port -a $password ttl "$line")
         if [[ $ttl_result == -1 ]];then
             echo 'key:'"$line" >> ./redis_modify_key/modify_key_"$exec_time".log
             # 获取5位不为0开始的随机数，避免设置同一过期时间，引起缓存雪崩
@@ -42,19 +47,17 @@ function modifyKeyTime(){
                 echo 'result:'"$result"
             } >> ./redis_modify_key/modify_key_"$exec_time".log
         fi
-    done
+    done < scan_result
 
     # 以 0 作为游标开始一次新的迭代， 一直调用 SCAN 命令， 直到命令返回游标 0 ，遍历完毕
-    while [ $cursor -ne $new_cursor ]
-    do
-        ./redis-cli -h $db_ip -p $db_port -a $password scan $new_cursor count $cnt > scan_tmp_result
-        new_cursor=`sed -n '1p' scan_tmp_result`
+    while [ $cursor -ne "$new_cursor" ]; do
+        ./redis-cli -h $db_ip -p $db_port -a $password scan "$new_cursor" count $cnt > scan_tmp_result
+        new_cursor=$(sed -n '1p' scan_tmp_result)
         sed -n '2,$p' scan_tmp_result > scan_result
-        cat scan_result |while read line
-        do
-            ttl_result=`./redis-cli -h $db_ip -p $db_port -a $password ttl $line`
+        while read -r line < scan_result; do
+            ttl_result=$(./redis-cli -h $db_ip -p $db_port -a $password ttl "$line")
             if [[ $ttl_result == -1 ]];then
-                echo 'key:'$line >> ./redis_modify_key/modify_key_$exec_time.log
+                echo 'key:'"$line" >> ./redis_modify_key/modify_key_"$exec_time".log
                 # 获取5位不为0开始的随机数，避免设置同一过期时间，引起缓存雪崩
                 time=$(tr -cd '1-9' </dev/urandom | head -c 5)
                 # 获取key的值
@@ -67,10 +70,8 @@ function modifyKeyTime(){
                     echo 'result:'"$result"
                 } >> ./redis_modify_key/modify_key_"$exec_time".log
             fi
-        done
+        done < scan_result
     done
-    rm -rf scan_tmp_result
-    rm -rf scan_result
 }
 
 # 脚本主函数（入口）
@@ -80,6 +81,7 @@ function main(){
     
     mkdirs
     modifyKeyTime
+    cleanup
     
     echo "[$(date "+%Y-%m-%d %H:%M:%S")]  done!"
 }

03d6814e6dee52a8a32d03a0b4e968fa183d4f74
不忘初心 <1624717079@qq.com>
2024-07-15 16:28:56 +0800

    Update git_tag_del.sh

--- devops/git_tag_del.sh
+++ devops/git_tag_del.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
 # 删除旧的tag，保留最近三个月。
 # author YaoKun
-set -ex
 
-month=$(date +%Y%m)
-month1=$(date -d "${month}01 last month" +%Y%m)
-month2=$(date -d "${month1}01 last month" +%Y%m)
+# 获取当前月份、上个月份和上上个月份的月份代码（格式为YYYYMM）
+current_month=$(date +%Y%m)
+last_month=$(date -d "1 month ago" +%Y%m)
+second_last_month=$(date -d "2 months ago" +%Y%m)
 
-# 清理历史tag
-echo "保留以下月份tag:${month},${month1},${month2}"
+# 将标签名写入临时文件，但排除最近三个月的标签
+git tag --list | grep -v "^${current_month}" | grep -v "^${last_month}" | grep -v "^${second_last_month}" > tmp.txt
 
-#git show-ref --tag | grep -v ${month} | grep -v ${month1} | grep -v ${month2} | awk '{print ":" $2}' > tmp.txt
+# 读取临时文件中的每个标签名，并删除远程仓库中的对应标签
+while IFS= read -r tag_name; do
+    git push origin --delete "$tag_name"
+done < tmp.txt
 
-git show-ref --tag | \
-grep -v ${month} |   \
-grep -v ${month1} |  \
-grep -v ${month2} |  \
-awk '{print ":" $2}' > tmp.txt
-
-file=tmp.txt
-
-for i in `cat ${file}`
-do
-git push origin $i;
-done;
+# 清理临时文件
 rm -f tmp.txt
-
-# 同步远程tag到本地
-#git fetch --prune --tags

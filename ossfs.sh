e340e2e0c865f999a8bf53c4646261fd03563dbf
不忘初心 <1624717079@qq.com>
2024-09-23 15:48:07 +0800

    Update ossfs.sh

--- ossfs.sh
+++ ossfs.sh
@@ -1,20 +1,8 @@
-#!/bin/bash
+#! /bin/bash
+#
+# ossfs      Automount Aliyun OSS Bucket in the specified direcotry.
+#
+# chkconfig: 2345 90 10
+# description: Activates/Deactivates ossfs configured to start at boot time.
 
-# 配置阿里云OSS信息
-OSS_BUCKET="backup"
-OSS_MOUNT_POINT="/data/oss"
-OSS_ENDPOINT="http://oss-cn-shanghai.aliyuncs.com"
-
-# 检查是否已经挂载
-if grep -qs "$OSS_MOUNT_POINT" /proc/mounts; then
-    echo "OSSFS已经挂载在$OSS_MOUNT_POINT"
-else
-    echo "挂载OSSFS到$OSS_MOUNT_POINT"
-    ossfs "$OSS_BUCKET" "$OSS_MOUNT_POINT" -ourl="$OSS_ENDPOINT"
-    
-    if [ $? -eq 0 ]; then
-        echo "成功挂载OSSFS到$OSS_MOUNT_POINT"
-    else
-        echo "挂载OSSFS失败"
-    fi
-fi
+ossfs backup /data/ossfs -ourl=http://oss-cn-shanghai-internal.aliyuncs.com -oallow_other -ouid=1000 -ogid=1000

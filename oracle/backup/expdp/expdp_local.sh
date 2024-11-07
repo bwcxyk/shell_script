6147368cc078f4b0f185abe528c3a03fafe6777b
不忘初心 <1624717079@qq.com>
2024-11-07 13:49:46 +0800

    Update expdp_local.sh

--- oracle/backup/expdp/expdp_local.sh
+++ oracle/backup/expdp/expdp_local.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
-##auth expdp
+# expdp
+# author:yaokun
 
 #set -ex
 
 # shellcheck source=/dev/null
 source /home/oracle/.bash_profile
-date=$(date +'%Y%m%d%H%M')
+currentTimestamp=$(date +'%Y%m%d%H%M')   # 当前精确到分钟的时间戳
+yesterdayDate=$(date -d yesterday +'%Y%m%d')  # 昨天的日期
 user='user'
 passwd='passwd'
 application="tms"
 directory="DATA_PUMP_DIR"
-olddate=$(date -d yesterday +'%Y%m%d')
 backdir="/data/oracle/admin/orcl/dpdump"
 
 function backup {
-    if expdp ${user}/${passwd} \
-        directory=${directory} \
-        schemas=${user} \
+    if expdp "${user}/${passwd}" \
+        directory="${directory}" \
+        schemas="${user}" \
         exclude=statistics \
-        exclude=table:\"= \'SYSTEM_LOG\'\" \
-        exclude=table:\"IN\(\'SHIP_B0923\',\'TMS_LOG\'\)\" \
-        exclude=table:\"LIKE\ \'SYSTEM_LOG%\'\" \
         filesize=2048M \
         parallel=2 \
-        dumpfile="${application}"_"${date}"_%U.dmp \
+        dumpfile="${application}"_"${currentTimestamp}"_%U.dmp \
         compression=all
     then
         echo -e "\033[34;1m导出成功  \033[0m"
@@ -34,9 +32,10 @@ function backup {
 }
 
 function del_old {
-    echo "start delete"
-    cd ${backdir} || exit
-    rm -fv "${application}"_"${olddate}"*.dmp
+    echo "开始删除昨日备份文件"
+    cd "${backdir}" || { echo "无法进入目录 ${backdir}"; exit 1; }
+    rm -fv "${application}"_"${yesterdayDate}"*.dmp
+    echo "删除完成"
 }
 
 function main {
